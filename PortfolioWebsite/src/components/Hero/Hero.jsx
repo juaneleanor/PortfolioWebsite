bf306f5a43c3c8d642cb6b90c502a14ea56705fd
Eleanor Janine Juan <emjuan@up.edu.ph>
2024-08-14 11:38:39 +0800

    Hero Section

---
+++ PortfolioWebsite/src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Ellie</h1>
+        <p className={styles.description}>
+          Passionate Computer Science Graduate Aspiring to be a Software
+          Engineer and Data Scientist – Reach Out If You'd Like to Learn More!
+        </p>
+        <a href="mailto:juaneleanor@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero imahge of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur}></div>
+      <div className={styles.bottomBlur}></div>
+    </section>
+  );
+};
