348688511bb10331b7803b01ccd4b2b0e199b529
Eleanor Janine Juan <emjuan@up.edu.ph>
2024-08-18 00:33:26 +0800

    Contact

---
+++ PortfolioWebsite/src/components/Contact/Contact.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email Icon" />
+          <a href="mail:to juaneleanor@gmail.com">juaneleanor@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn Icon"
+          />
+          <a href="https://www.linkedin.com/in/juaneleanor">
+            linkedin.com/in/juaneleanor
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="GitHub Icon" />
+          <a href="https://github.com/juaneleanor">github.com/juaneleanor</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
